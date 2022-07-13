8cef3d88a18e6fbcabeca7a8108323fbb6249f49
kozlov artem <artemko1987@yandex.ru>
2022-07-13 06:22:58 +0300

    add Zadacha5(16)

---
+++ Zadacha5(16)/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход 2 числа
+//и проверяет, является ли одно число квадратов другого
+//5, 25 -> да
+//-4, 16 -> да
+//25, 5 -> да
+//8, 9 -> нет
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b || b * b == a)
+    Console.WriteLine("да");
+else 
+    Console.WriteLine("Нет");
